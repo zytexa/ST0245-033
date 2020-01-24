7b1992d57bf3606f77eaf3903b007d54d0ba4157
zytexa <51744054+zytexa@users.noreply.github.com>
2020-01-24 15:09:02 -0500

    Update Taller1.py

--- talleres/taller01/Taller1.py
+++ talleres/taller01/Taller1.py
@@ -1,30 +1,32 @@
 import math
 class Punto2D:
-    """Representacion de punto en 2 dimensiones"""
+  """Representacion de punto en 2 dimensiones"""
 
-    def __init__(self, x, y):
-      self.x = x
-      self.y = y
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
 
-    def get_x(self):
-      return self.x
+  def get_x(self):
+    return self.x
 
-    def get_y(self):
-      return self.y
+  def get_y(self):
+    return self.y
 
-    def polar_radius(self):
-      return math.sqrt(x*x + y*y)
+  def polar_radius(self):
+    return math.sqrt(self.x*self.x + self.y*self.y)
 
-    def polar_angle(self, x, y):
-      angulo = math.atan2(y,x)
-      return math(angulo)
+  def polar_angle(self):
+    angulo = math.atan2(self.y,self.x)
+    return (angulo)
 
-    def dist_euclidiana(self, other):
-      return math.sqrt(((self.x-other.x)**2)+((self.y-other.y)**2))
+  def euclidian_dist(self, other):
+    return math.sqrt(((self.x-other.x)**2)+((self.y-other.y)**2))
 
-punto_1 = Punto2D(10,20)
-punto_2 = Punto2D(0,0)
+origen = Punto2D(0,0)
+punto = Punto2D(10,20)
 
-punto_1.dist_euclidiana(punto_2)
-
-punto_1.polar_angle(10,20)
+print('X value is',str(punto.get_x()))
+print('Y value is',str(punto.get_y()))
+print('The polar radius is',str(punto.polar_radius()),'units long.')
+print('The polar angle measures',str(punto.polar_angle()),'radians.')
+print('The euclidian distance between the point and the origin is',str(punto.euclidian_dist(origen)),'units long.')
