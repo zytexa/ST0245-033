5760d1081e631d9c3e97e3792e5c0f77c12eb13d
zytexa <51744054+zytexa@users.noreply.github.com>
2020-01-24 17:50:54 -0500

    Update Taller1.py

--- talleres/taller01/Taller1.py
+++ talleres/taller01/Taller1.py
@@ -1,3 +1,4 @@
+#Punto 1
 import math
 class Punto2D:
   """Representacion de punto en 2 dimensiones"""
@@ -30,3 +31,28 @@ print('Y value is',str(punto.get_y()))
 print('The polar radius is',str(punto.polar_radius()),'units long.')
 print('The polar angle measures',str(punto.polar_angle()),'radians.')
 print('The euclidian distance between the point and the origin is',str(punto.euclidian_dist(origen)),'units long.')
+
+#Punto 2
+class Date:
+  def __init__(self, day, month, year):
+    self.day = day
+    self.month = month
+    self.year = year
+
+  def __str__(self):
+    return str(self.day) + "-" + str(self.month) + "-" + str(self.year)
+
+  def compare_dates(self, date):
+    if date.year == self.year and date.month == self.month and date.day == self.day:
+        return 'This date is the same.'
+    if date.year > self.year or date.year == self.year and date.month > self.month or date.year == self.year and date.month == self.month and date.day > self.day:
+        return 'This date is after.'
+    return 'This date is before'
+
+#A = Date(23,1,1997)
+#B = Date(23,2,1997)
+#C = Date(10,8,1996)
+#D = Date(23,1,1997)
+#A.compare_dates(B)
+#A.compare_dates(D)
+#A.compare_dates(C)
